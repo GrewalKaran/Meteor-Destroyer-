d9e21273f00ec4994d924f0a0a87a9e523a1336b
Karan grewal <karangrewal979@gmail.com>
2025-05-13 02:04:10 +0530

    Fix asset paths for Vite public folder

--- scenes/loadingScene.js
+++ scenes/loadingScene.js
@@ -6,22 +6,19 @@ export default class  LoaderScene extends Phaser.Scene{
     }
 
     preload(){
-    this.load.image('player', 'public/assets/player.png')
-    this.load.image('laser', 'public/assets/laserBlue16.png')
-    this.load.image('meteor-small', 'public/assets/meteor_small.png')
-    this.load.image('meteor-med', 'public/assets/meteor_med.png')
-    this.load.image('meteor-large', 'public/assets/meteor_large.png')
+        this.load.image('player', 'assets/player.png')
+        this.load.image('laser', 'assets/laserBlue16.png')
+        this.load.image('meteor-small', 'assets/meteor_small.png')
+        this.load.image('meteor-med', 'assets/meteor_med.png')
+        this.load.image('meteor-large', 'assets/meteor_large.png')
+        this.load.image('starfield', 'assets/starfield_800x600.jpg')
+        
+        this.load.bitmapFont('arcade', 'assets/Fonts/arcade.png', 'assets/Fonts/arcade.xml')
+        
+        
 
-    this.load.image('starfield', 'public/assets/starfield_800x600.jpg')
-
-    this.load.bitmapFont(
-            "arcade",
-            "assets/Fonts/arcade.png",  
-            "assets/Fonts/arcade.xml"
-);
-
-    this.load.audio('shoot','assets/laser-shoot.wav')
-    this.load.audio('explosion','assets/laser-explosion.wav')
+        this.load.audio('shoot', 'assets/laser-shoot.wav')
+        this.load.audio('explosion', 'assets/laser-explosion.wav')
     
 }
     
